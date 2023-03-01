67d0ca816eeb50278491848fb9a15f14764f9925
vasu <vasuparsaniya21@gmail.com>
2023-03-01 08:23:25 +0530

    inital commmit

--- app.js
+++ app.js
@@ -34,9 +34,8 @@ const port = process.env.PORT || 5000;
 // });
 // app.use(cors());
 app.use(express.json());
-app.get("/", function (req, res) {
-  res.send("hi");
-});
+
+
 
 app.post("/login", (req, res) => {
   const mail = req.body.mail;
